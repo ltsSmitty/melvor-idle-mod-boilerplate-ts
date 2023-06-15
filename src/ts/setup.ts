f62d633857eda3ca6d9c1a6793ee2628b099f4b3
Smitty Penman <smitty.penman@gmail.com>
2023-06-14 20:17:31 -0400

    initial release

--- src/ts/setup.ts
+++ src/ts/setup.ts
@@ -5,6 +5,8 @@ import Greeter from '../components/Greeter/Greeter';
 // Data
 // Game data for registration
 import ModData from '../data/data.json';
+// import ModData2 from '../data/regularSpeedrunData.json';
+
 
 // Styles
 // Will automatically load your styles upon loading the mod
@@ -15,30 +17,101 @@ import '../css/styles.css';
 import '../img/icon.png';
 // Reference images using `ctx.getResourceUrl`
 import LargeIcon from '../img/icon_large.png';
+import { xpForLevel } from './xpTable';
+
+const skills = [
+  [game.attack, Attack],
+  [game.strength, Strength],
+  [game.defence, Defence],
+  [game.hitpoints, Hitpoints],
+  [game.ranged, Ranged],
+  [game.altMagic, AltMagic],
+  [game.prayer, Prayer],
+  [game.slayer, Slayer],
+  [game.farming, Farming],
+  [game.woodcutting, Woodcutting],
+  [game.fishing, Fishing],
+  [game.firemaking, Firemaking],
+  [game.cooking, Cooking],
+  [game.mining, Mining],
+  [game.smithing, Smithing],
+  [game.thieving, Thieving],
+  [game.fletching, Fletching],
+  [game.crafting, Crafting],
+  [game.herblore, Herblore],
+  [game.agility, Agility],
+  [game.summoning, Summoning],
+  [game.runecrafting, Runecrafting],
+  [game.astrology, Astrology],
+] as const;
+
 
 export async function setup(ctx: Modding.ModContext) {
   // Register our GameData
-  await ctx.gameData.addPackage(ModData);
+  await ctx.gameData.addPackage(ModData as any);
 
-  // Because we're loading our templates.min.html file via the manifest.json,
-  // the templates aren't available until after the setup() function runs
-  ctx.onModsLoaded(() => {
-    const root = document.createElement('div');
-    ui.create(Greeter({ name: 'Melvor' }), root);
+  ctx.onCharacterLoaded(() => {
 
-    sidebar.category('Modding').item('Mod Boilerplate', {
-      icon: ctx.getResourceUrl('img/icon.png'),
-      onClick() {
-        open(ctx, root);
-      },
-    });
-  });
+    if (game.currentGamemode.namespace !== "reverse_xp_speedrun") return;
+
+    if (game.attack.xp === 0 && game.completion.totalProgressBaseGame < 1) {
+      console.log('Fresh Account');
+      skills.forEach(s => {
+        const skill = s[0];
+        console.log(skill.name, skill.xp, skill.level);
+        // skill.addXP(-(xpForLevel(100) - 1))
+        skill.addXP(-(xpForLevel(98)) - 2)
+        console.log(skill.name, skill.xp, skill.level);
+      })
+    }
+
+    skills.forEach(s => {
+      const [skill, SkillClass] = s;
+      console.log("Patching", skill.name, skill.xp, skill.level);
+
+      ctx.patch<typeof SkillClass, "level">(SkillClass, "level").get((original) => {
+        // unclear why the level is one too hot, but it is
+        const currentLevel = original() - 1;
+        if (skill.xp < xpForLevel(currentLevel)) {
+          console.log('downleveling ', skill.name);
+          return currentLevel - 1;
+        }
+        return original();
+      })
+
+      // // if the xp is less than the amount for the level, perform the downlevel
+      // ctx.patch(Fishing, "level").get((original) => {
+
+      //   const currentLevel = original() - 1;
+      //   if (game.fishing.xp < xpForLevel(currentLevel)) {
+      //     console.log('downlevel fishing');
+      //     return currentLevel - 1;
+      //   }
+      //   return original();
+      // })
+    })
+  }
+  )
 }
+    // Because we're loading our templates.min.html file via the manifest.json,
+    // the templates aren't available until after the setup() function runs
+    // ctx.onModsLoaded(() => {
+    //   const root = document.createElement('div');
+    //   ui.create(Greeter({ name: 'Melvor' }), root);
 
-function open(ctx: Modding.ModContext, html: HTMLElement) {
-  SwalLocale.fire({
-    iconHtml: `<img class="mbts__logo-img" src="${ctx.getResourceUrl(LargeIcon)}" />`,
-    title: ctx.name,
-    html,
-  });
-}
+    //   sidebar.category('Modding').item('Mod Boilerplate', {
+    //     icon: ctx.getResourceUrl('img/icon.png'),
+    //     onClick() {
+    //       open(ctx, root);
+    //     },
+    //   });
+    // });
+
+
+// function open(ctx: Modding.ModContext, html: HTMLElement) {
+//   SwalLocale.fire({
+//     iconHtml: `<img class="mbts__logo-img" src="${ctx.getResourceUrl(LargeIcon)}" />`,
+//     title: ctx.name,
+//     html,
+  // });
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,59 @@
-const CopyPlugin = require('copy-webpack-plugin');
-const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/ts/setup.ts',
-  experiments: {
-    outputModule: true
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'setup.mjs',
-    path: path.resolve(__dirname, 'dist'),
-    library: {
-      type: 'module',
-    },
-    clean: true,
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        { from: 'manifest.json', to: 'manifest.json' },
-      ]
-    })
-  ],
-  module: {
-    generator: {
-      'asset/resource': {
-        publicPath: 'img/',
-        outputPath: 'img/',
-        filename: '[name][ext]',
-      },
-    },
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ]
-  },
+	mode: "production",
+	entry: "./src/ts/setup.ts",
+	experiments: {
+		outputModule: true,
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
+	output: {
+		filename: "setup.mjs",
+		path: path.resolve(__dirname, "dist"),
+		library: {
+			type: "module",
+		},
+		clean: true,
+	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [{ from: "manifest.json", to: "manifest.json" }],
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				exclude: path.resolve("/src/assets"),
+				type: "asset/resource",
+				generator: {
+					publicPath: "img/",
+					outputPath: "img/",
+					filename: "[name][ext]",
+				},
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				exclude: path.resolve("/src"),
+				type: "asset/resource",
+				generator: {
+					publicPath: "assets/",
+					outputPath: "assets/",
+					filename: "[name][ext]",
+				},
+			},
+		],
+	},
 };
